49fdd8153649ad289b7c9b990bc38f46766fe8e7
DevCarlosZz <112131250+DevCarloss@users.noreply.github.com>
2022-09-17 16:06:21 -0300

    Update api_node.js

--- api_node.js
+++ api_node.js
@@ -1,10 +1,20 @@
 const fs = require('fs')
-const app = require('app')
+var express = require('express')
+const app = express()
 console.log('Base Api NodeJS Criado Por CarlosDev')
 
-// App Use
+// App Get
 
-app.get('/' req,res => {
-    res.send('Api') 
+app.get('/', (req,res) => {
+    res.send('Forbidden')
         
 })
+
+// App Get Api Info
+
+app.get('/apiinfo',(req,res) => {
+  res.send('<h1>Interface de programação de aplicações<h1>' + '\n\n' + '<h3>As APIs são um conjunto de padrões que fazem parte de uma interface e que permitem a criação de plataformas de maneira mais simples e prática para desenvolvedores. A partir de APIs é possível criar softwares, aplicativos, programas e plataformas diversas. Por exemplo, apps desenvolvidos para celulares Android e iPhone (iOS) são criados a partir de padrões definidos e disponibilizados pelas APIs de cada sistema operacional.<h3>' + '\n\n' + '<img src = "https://peerbits-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/rest-api-code-main.png"><img>')
+  
+})
+
+app.listen(8000)
